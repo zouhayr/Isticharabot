3f0ef0ac69d586ece56053a82ccc967be6228125
zouhayr <zoohayrweb@gmail.com>
2025-03-08 13:06:15 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,21 @@
-
 #!/bin/bash
-echo "Current directory: $(pwd)"
-echo "Listing all files in /app:"
-ls -la /app
+set -x  # Affiche chaque commande avant exécution pour débogage
+echo "Starting entrypoint.sh"
+echo "Current directory: $(/bin/pwd)"
+echo "TELEGRAM_BOT_TOKEN: $TELEGRAM_BOT_TOKEN"
+echo "TELEGRAM_BOT_NAME: $TELEGRAM_BOT_NAME"
+echo "Listing files in /app:"
+/bin/ls -la /app
 echo "Listing files in /app/models:"
-ls -la /app/models/
-cat <<EOF > /app/credentials_temp.yml
+/bin/ls -la /app/models || echo "Error: /app/models not found"
+echo "Creating credentials_temp.yml"
+/bin/cat <<EOF > /app/credentials_temp.yml
 telegram:
   access_token: "$TELEGRAM_BOT_TOKEN"
   verify: "$TELEGRAM_BOT_NAME"
   webhook_url: "https://isticharabot-isticharachat.koyeb.app/webhooks/telegram/webhook"
 EOF
-exec rasa run --enable-api --cors "*" --port 5005 --connector telegram --credentials /app/credentials_temp.yml --model models/model.tar.gz
+echo "Credentials file created, content:"
+/bin/cat /app/credentials_temp.yml
+echo "Launching Rasa"
+/usr/local/bin/rasa run --enable-api --cors "*" --port 5005 --connector telegram --credentials /app/credentials_temp.yml --model models/model.tar.gz
