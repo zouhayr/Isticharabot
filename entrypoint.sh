dfc631cf56f76b1296c297208c2de2b25734ad40
zouhayr <zoohayrweb@gmail.com>
2025-03-08 15:58:28 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ telegram:
   verify: "$TELEGRAM_BOT_NAME"
   webhook_url: "https://isticharabot-isticharachat.koyeb.app/webhooks/telegram/webhook"
 EOF
+echo "Launching Rasa in background"
 rasa run --enable-api --cors "*" --port 5005 --connector telegram --credentials /app/credentials_temp.yml --model models/model.tar.gz &
 echo "Waiting for Rasa to be ready on port 5005..."
 until curl -s http://localhost:5005/ > /dev/null; do
-  echo "Still waiting..."
+  echo "Still waiting for Rasa..."
   sleep 5
 done
 echo "Rasa is running now"
