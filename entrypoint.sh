273df3a8821cf6aeac410a9c3e78dd91a059f1d2
zouhayr <zoohayrweb@gmail.com>
2025-03-08 10:33:06 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-# Écrire les variables dans un fichier de débogage
-echo "TELEGRAM_BOT_TOKEN: $TELEGRAM_BOT_TOKEN" > /tmp/debug.log
-echo "TELEGRAM_BOT_NAME: $TELEGRAM_BOT_NAME" >> /tmp/debug.log
+# Afficher les variables pour le débogage (visible dans les logs Koyeb)
+echo "TELEGRAM_BOT_TOKEN: $TELEGRAM_BOT_TOKEN"
+echo "TELEGRAM_BOT_NAME: $TELEGRAM_BOT_NAME"
+echo "Listing files in /app/models:"
+ls -la /app/models/
 
 # Créer un credentials.yml temporaire avec les valeurs substituées
 cat <<EOF > /app/credentials_temp.yml
 telegram:
   access_token: "$TELEGRAM_BOT_TOKEN"
   verify: "$TELEGRAM_BOT_NAME"
-  webhook_url: "TEMPORAIRE"  # À remplacer par l’URL finale
+  webhook_url: "https://isticharabot-isticharachat.koyeb.app/webhooks/telegram/webhook"
 EOF
 
-# Lancer Rasa avec le fichier temporaire
-exec rasa run --enable-api --cors "*" --port 5005 --connector telegram --credentials /app/credentials_temp.yml
+# Lancer Rasa avec le modèle et le fichier temporaire
+exec rasa run --enable-api --cors "*" --port 5005 --connector telegram --credentials /app/credentials_temp.yml --model models/model.tar.gz
